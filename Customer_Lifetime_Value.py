6a4f2ae6a7b464e59b207c81bb8f7c2ed358553f
Eromer55 <85547094+Eromer55@users.noreply.github.com>
2022-06-05 16:48:45 +0300

    Update and rename ._cltv.py to Customer_Lifetime_Value.py

--- Customer_Lifetime_Value.py
+++ Customer_Lifetime_Value.py
@@ -27,6 +27,7 @@ import datetime as dt
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 1000)
 #pd.set_option('display.max_rows, None)
 #Aşağıdaki kod çıktılarda ki sayısal değerlerin kaç decimal gözükmesini ayarlar
 pd.set_option('display.float_format', lambda x : '%.5f' % x)
@@ -57,21 +58,18 @@ cltv_c.columns = ['total_transaction', 'total_unit', 'total_price']
 # 2. Average Order Value (average_order_value = total_price / total_transaction)
 ###############################################################################################
 
-cltv_c.head()
-
 cltv_c['average_order_value'] = cltv_c["total_price"] / cltv_c['total_transaction']
 
 ##############################################################################################
 # 3. Satın Alma Sıklığı - Purchase Frequency (total_transaction / total_number_of_customers)
 ###############################################################################################
 
-cltv_c.head()
 cltv_c.shape
 cltv_c.shape[0]
 cltv_c['purchase_frequency'] = cltv_c["total_transaction"] / cltv_c.shape[0]
 
 ##############################################################################################
-# 4. Repeat Rate & Churn Rate (birden fazla alışveriş yapan müşteri sayısı / tüm müşteriler) - Tekrarlama ve Kaybetme Oranı
+# 4. Repeat Rate & Churn Rate (birden fazla alışveriş yapan müşteri sayısı / tüm müşteriler)
 ###############################################################################################
 
 cltv_c.head()
@@ -81,7 +79,7 @@ repeat_rate = cltv_c[cltv_c['total_transaction'] > 1].shape[0] / cltv_c.shape[0]
 churn_rate = 1 - repeat_rate
 
 ##############################################################################################
-# 5. Profit Margin (profit_margin = total_price * 0.10)- Kar Marjı
+# 5. Profit Margin (profit_margin = total_price * 0.10)
 ###############################################################################################
 
 cltv_c.head()
@@ -89,13 +87,13 @@ cltv_c.head()
 cltv_c['profit_margin'] = cltv_c['total_price'] * 0.10
 
 ##############################################################################################
-# 6. Customer Value (customer_value = average_order_value * purchase_frequency)- Müşteri Değeri
+# 6. Customer Value (customer_value = average_order_value * purchase_frequency)
 ###############################################################################################
 
 cltv_c['customer_value'] = cltv_c['average_order_value'] * cltv_c['purchase_frequency']
 
 ##############################################################################################
-# 7. Customer LifeTime Value (CLTV = (customer_value / churn_rate) * profit_margin)- Müşteri Yaşam Boyu Değeri
+# 7. Customer LifeTime Value (CLTV = (customer_value / churn_rate) * profit_margin)
 ###############################################################################################
 
 cltv_c['cltv'] = (cltv_c['customer_value'] / churn_rate) * cltv_c['profit_margin']
